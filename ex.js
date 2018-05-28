80ba74026284a16cedb1b60e31ec5d85a972afd7
Yihyeon Kwon <yhkwon@tyle.io>
2018-05-28 18:02:01 +0900

    add ex

---
+++ ex.js
@@ -0,0 +1,7 @@
+
+var page = require('webpage').create();
+
+page.open("http://slimerjs.org", function (status) {
+	page.viewportSize = { width:1024, height:768 };
+	page.render('screenshot.png')
+});
