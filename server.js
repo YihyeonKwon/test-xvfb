85135735a6277ecd0eb69ce0c918ed144f04a835
Yihyeon Kwon <yhkwon@tyle.io>
2018-08-03 17:21:26 +0900

    add ex

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var path = require('path');
 var fs = require('fs');
-
+var webdriver = require('selenium-webdriver');
+var By = webdriver.By;
+var chrome = require('selenium-webdriver/chrome');
+var chromeOption = new chrome.Options();
+var driver = null;
+require('chromedriver');// 2.32.3
 
 var port = normalizePort(process.env.PORT || 3000);
 app.set('port', port);
@@ -45,7 +50,7 @@ app.get('/api/start', function (req, res) {
 
 	xvrf = spawn('xvfb-run', ['--listen-tcp', '--server-num', '44', '-s', '"-ac -screen 0 1920x1080x24"', 'google-chrome', '--window-size=1920,1080', '--start-fullscreen', '--disable-infobars', '--disable-notifications', 'https://tyle.io/player/r4goi5fmzwgox7', '>', '/dev/null'], {stdio: 'inherit'});
 
-	setTimout(function () {
+	setTimeout(function () {
 		ffmpeg = spawn('ffmpeg', ['-t', 10, '-y', '-f', 'x11grab', '-draw_mouse', 0, '-video_size', '900x900', '-i', ':44+0,120', '-vcodec', 'libx264', '-pix_fmt', 'yuv420p', '-preset', 'ultrafast', '-r', 60, '-crf', 15, '-tune', 'zerolatency', '-filter:a', 'volume=1.0', '-c:a', 'aac', '-strict', 'experimental', '-ac', 2, '-b:a', '192k', __dirname + '/test_10.mp4'], {stdio: 'inherit'});
 
 		res.sendStatus(200);
@@ -104,6 +109,31 @@ server.on('listening', onListening);
 function onListening() {
 	console.log('onListening');
 
+	var headless = require('headless');
+	headless(options, function(err, childProcess, servernum) {
+		console.log('Xvfb running on server number', servernum);
+		console.log('Xvfb pid', childProcess.pid);
+		console.log('err should be null', err);
+
+
+		chromeOption.addArguments('--start-fullscreen');
+		chromeOption.addArguments('--disable-infobars');
+		chromeOption.addArguments('--disable-notifications');
+
+		driver = new webdriver.Builder()
+		// driver.Chrome('path to /chromedriver')
+		.forBrowser('chrome')
+		.setChromeOptions(chromeOption)
+		.build();
+
+		setTimeout(function () {
+
+			driver.get('https://tyle.io/player/r4goi5fmzwgox7');
+
+			ffmpeg = spawn('ffmpeg', ['-t', 10, '-y', '-f', 'x11grab', '-draw_mouse', 0, '-video_size', '1400x900', '-i', 'localhost:4444+0,120', '-vcodec', 'libx264', '-pix_fmt', 'yuv420p', '-preset', 'ultrafast', '-r', 60, '-crf', 15, '-tune', 'zerolatency', '-filter:a', 'volume=1.0', '-c:a', 'aac', '-strict', 'experimental', '-ac', 2, '-b:a', '192k', __dirname + '/test_10.mp4'], {stdio: 'inherit'});
+		}, 1000);
+	});
+
 	setTimeout(function () {
 		// var proc = exec('xvfb-run --listen-tcp --server-num 44 --auth-file /tmp/xvfb.auth -s "-ac -screen 0 1920x1080x24" google-chrome --window-size=1920,1080 --start-fullscreen --disable-infobars --disable-notifications https://tyle.io/player/r4goi5fmzwgox7 > /dev/null', function (err, stdout, stderr) {
 		// 	if (err) {
@@ -136,6 +166,8 @@ function onListening() {
 		// 		console.log('exec error: ' + error);
 		// 	}
 		// });
+
+
 
 	}, 500)
 }
